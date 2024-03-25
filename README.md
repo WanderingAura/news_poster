070ff756b341369447270ae0ddd9678da87e8123
volcanoeyes <130888597+volcanoeyes@users.noreply.github.com>
2024-03-25 22:14:38 +0800

    add main logic

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 A command line app that scrapes the latest headlines and articles from the BBC news website and gives you the option of
 emailing the articles to your email or tweeting the headline to a predefined twitter account. It also gives you the
 option to browse the articles inside of the command line.
+
+This is built on top of some code I wrote in this [respository](https://github.com/BenjaminRedhead/TwitBot), an
+unfinished project where the goal was to build a twitter bot which uses the ChatGPT API to summarise the currently
+trending topics.
--- app/main.py
+++ app/main.py
@@ -4,14 +4,27 @@ import keys
 
 
 def main():
-    print("What do you want to do?\n"
-          "1. Send the 5 biggest news articles to me in email\n"
-          "2. Tweet the biggest headline\n"
-          "3. Browse the headlines\n"
-          "4. Quit\n")
-    option = input("Please enter a number: ")
+    while True:
+        print("What do you want to do?\n"
+              "1. Send the 5 biggest news articles to me in email\n"
+              "2. Tweet the biggest headline\n"
+              "3. Browse the headlines\n"
+              "4. Quit")
+        option = int(input("Please enter a number: "))
 
-    options_dict = {"1": email_articles}
+        match option:
+            case 1:
+                email_articles()
+            case 2:
+                tweet_headline()
+            case 3:
+                print_headlines()
+            case 4:
+                break
+            case _:
+                print("Please enter one of the options")
+                continue
+        print("Operation successful")
 
 
 def email_articles(articles_dict, n=5):
@@ -26,6 +39,19 @@ def email_articles(articles_dict, n=5):
     gmail = Gmail(keys.EMAIL_SENDER, keys.EMAIL_SENDER_PASSWORD)
     gmail.send(email, "\n\n".join(articles[0:n]))
     return 0
+
+
+def tweet_headline(headline):
+    twitter = Twitter()
+    twitter.post(headline)
+    return 0
+
+
+def print_headlines(articles_dict):
+    articles = []
+    for h, text in articles_dict:
+        articles.append("1. " + h + "\n" + text)
+    print("\n\n".join(articles))
 
 
 if __name__ == "__main__":
